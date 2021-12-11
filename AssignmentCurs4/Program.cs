2d71b6412d13f9a8773c7243730f383d42c7f94c
Bogdan748 <andriesbogdan00@gmail.com>
2021-12-11 12:45:57 +0200

    Add project files.

---
+++ AssignmentCurs4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+
+namespace AssignmentCurs4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text;
+
+            bool b = citeste() || citeste() || citeste() ;
+
+            text = String.IsNullOrWhiteSpace(text) ? "Nu am continut" : $"Textul introdus este: {text}";
+
+            Console.WriteLine(text);
+
+
+            bool citeste()
+            {
+                text = Console.ReadLine();
+                return !String.IsNullOrWhiteSpace(text);
+            }
+        }
+    }
+}
